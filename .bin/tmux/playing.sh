8dbe36f79255c3e1de4430c1c11b64f6da01690b
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-11-21 10:43:46 +0330

    trim tmux playing string

--- .bin/tmux/playing.sh
+++ .bin/tmux/playing.sh
@@ -2,11 +2,11 @@
 
 INFO="$(mocp -i &2>/dev/null)"
 
-STATE="$(echo "$INFO" | awk '/^State/ { print $2 }')"
-TITLE="$(echo "$INFO" | awk '/^SongTitle/ { printf "%s %s", $2, $3 }')"
-ARTIST="$(echo "$INFO" | awk '/^Artist/ { printf "%s %s", $2, $3 }')"
-ELAPSED="$(echo "$INFO" | awk '/^CurrentTime/ { print $2 }')"
-TOTAL="$(echo "$INFO" | awk '/^TotalTime/ { print $2 }')"
+STATE="$(echo "$INFO" | awk '/^State/ { print $2 }' | xargs)"
+TITLE="$(echo "$INFO" | awk '/^SongTitle/ { printf "%s %s", $2, $3 }' | xargs)"
+ARTIST="$(echo "$INFO" | awk '/^Artist/ { printf "%s %s", $2, $3 }' | xargs)"
+ELAPSED="$(echo "$INFO" | awk '/^CurrentTime/ { print $2 }' | xargs)"
+TOTAL="$(echo "$INFO" | awk '/^TotalTime/ { print $2 }' | xargs)"
 
 case "$STATE" in
     STOP)
