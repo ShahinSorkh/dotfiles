15d3990b093cd43a493ab4cfb2ad2762bbe9481d
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-12-09 15:19:17 +0330

    use a bash script to toggle music (mocp) status

---
+++ .bin/tmux/toggle_music.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+STATE="$(echo "$(mocp -i &2>/dev/null)" | awk '/^State/ { print $2 }' | xargs)"
+
+case "$STATE" in
+    STOP)
+        mocp -p 2>&1 >/dev/null
+        ;;
+    PLAY)
+        mocp -P 2>&1 >/dev/null
+        ;;
+    PAUSE)
+        mocp -U 2>&1 >/dev/null
+        ;;
+    *)
+        mocp -S 2>&1 >/dev/null
+        mocp -p 2>&1 >/dev/null
+        ;;
+esac
+
+exit 0
