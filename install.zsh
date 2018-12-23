2ec9e5f3a62fff03dc41dd2c217b0ea6329c32e7
Shahin Sorkh <sorkh.shahin@hotmail.com>
2018-12-23 12:55:01 +0330

    more informative logs on install

--- install.zsh
+++ install.zsh
@@ -12,13 +12,13 @@ echo "PWD:\t$PWD"
 
 for f in $DOTFILES; do
     if [ -e "$HOME/$f" ]; then
-        echo "rm $HOME/$f"
+        echo "- rm $f"
         if [ -f "$HOME/$f" ]; then rm -rf "$HOME/$f"; fi
         if [ -d "$HOME/$f" ]; then rm -rf "$HOME/$f"; fi
         if [ -L "$HOME/$f" ]; then unlink "$HOME/$f"; fi
     fi
     if [ -e "$HOME/.$f" ]; then
-        echo "rm $HOME/.$f"
+        echo "- rm .$f"
         if [ -f "$HOME/.$f" ]; then rm -rf "$HOME/.$f"; fi
         if [ -d "$HOME/.$f" ]; then rm -rf "$HOME/.$f"; fi
         if [ -L "$HOME/.$f" ]; then unlink "$HOME/.$f"; fi
@@ -27,16 +27,16 @@ for f in $DOTFILES; do
     if [ "$f" = "moc_config" ]; then
         mkdir -p $HOME/.moc
         if [ -e "$HOME/.moc/config" ]; then
-            echo "rm $HOME/.moc/config"
+            echo "- rm .moc/config"
             if [ -f "$HOME/.moc/config" ]; then rm -rf "$HOME/.moc/config"; fi
             if [ -d "$HOME/.moc/config" ]; then rm -rf "$HOME/.moc/config"; fi
             if [ -L "$HOME/.moc/config" ]; then unlink "$HOME/.moc/config"; fi
         fi
 
-        echo "ln -s ../$DOTDIR/$f .moc/config"
+        echo "+ ln -s ../$DOTDIR/$f .moc/config"
         ln -s ../$DOTDIR/$f .moc/config
     else
-        echo "ln -s $DOTDIR/$f .$f"
+        echo "+ ln -s $DOTDIR/$f .$f"
         ln -s $DOTDIR/$f .$f
     fi
 done
