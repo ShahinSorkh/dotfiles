a7c1f175823ca6bd87167a91ba254fd00a9fbd62
Shahin Sorkh <sorkh.shahin@protonmail.com>
2019-09-24 09:48:48 +0330

    update zsh aliases

--- dot/zsh/plugins/common-aliases/common-aliases.plugin.zsh
+++ dot/zsh/plugins/common-aliases/common-aliases.plugin.zsh
@@ -3,17 +3,18 @@
 #
 
 # ls, the common ones I use a lot shortened for rapid fire usage
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias l='ls --group-directories-first -lFh'     #size,show type,human readable
+alias la='ls --group-directories-first -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
-alias ldot='ls -ld .*'
+alias ll='ls --group-directories-first -l'      #long list
+alias ldot='ls --group-directories-first -ld .*'
 alias lS='ls -1FSsh'
-alias lart='ls -1Fcart'
+alias lart='ls -1FcArt'
 alias lrt='ls -1Fcrt'
 
-alias zshrc='${=EDITOR} ~/.zshrc' # Quick access to the ~/.zshrc file
+alias zshrc='${=EDITOR} ~/.zshrc'  # Quick access to the ~/.zshrc file
+alias vimrc='${=EDITOR} ~/.vimrc'  # Quick access to the ~/.vimrc file
 
 # alias grep='grep --color'
 # alias sgrep='grep -R -n -H -C 5 --exclude-dir={.git,.svn,CVS} '
@@ -24,13 +25,13 @@ alias t='tail -f'
 alias -g H='| head'
 alias -g T='| tail'
 alias -g G='| egrep --color'
-alias -g L="| less"
-alias -g M="| most"
-alias -g LL="2>&1 | less"
-alias -g CA="2>&1 | cat -A"
-alias -g NE="2> /dev/null"
-alias -g NUL="> /dev/null 2>&1"
-# alias -g P="2>&1| pygmentize -l pytb"
+alias -g L='| less'
+alias -g M='| most'
+alias -g LL='2>&1 | less'
+alias -g CA='2>&1 | cat -A'
+alias -g NE='2> /dev/null'
+alias -g NUL='> /dev/null 2>&1'
+alias -g P='2>&1| pygmentize -l pytb'
 alias -g X='| xargs'
 alias -g A='| awk'
 
@@ -40,11 +41,11 @@ alias fd='find . -type d -name'
 alias ff='find . -type f -name'
 
 alias h='history'
-alias hgrep="fc -El 0 | egrep"
-alias help='man'
-alias p='ps -f'
+alias hgrep='fc -El 0 | grep -E'
+#alias help='man'
+alias p='ps -aux'
 alias sortnr='sort -n -r'
-alias unexport='unset'
+#alias unexport='unset'
 
 alias rm='rm -i'
 alias cp='cp -i'
