e8d832bf22946105912802a5abbf3256bf523d11
Shahin Sorkh <sorkh.shahin@protonmail.com>
2020-11-02 09:17:10 +0330

    use prelude to load slime on emacs

--- dot/emacs.d/personal/slime.el
+++ dot/emacs.d/personal/slime.el
@@ -1,6 +1,5 @@
-;; (prelude-require-package 'slime)
-
 (setq inferior-lisp-program "/usr/bin/sbcl")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
-(require 'slime)
+
+(prelude-require-package 'slime)
+
 (slime-setup '(slime-fancy))
