7c0ada56b50406c5a8936e26f5870c549a0e42b3
Shahin Sorkh <sorkh.shahin@protonmail.com>
2021-08-31 20:14:33 +0430

    install emacs-impostman

--- dot/doom.d/packages.el
+++ dot/doom.d/packages.el
@@ -9,7 +9,8 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 (package! graphql-mode)
-;(package! impostman)
+(package! impostman
+    :recipe (:host github :repo "flashcode/impostman"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
