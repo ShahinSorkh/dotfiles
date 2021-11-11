4b87a0503f7e9506917d0b37420b11f879f14cf2
Shahin Sorkh <sorkh.shahin@protonmail.com>
2021-11-11 03:54:51 +0330

    add basic startup hooks

--- dot/xmonad/lib/My/Hooks.hs
+++ dot/xmonad/lib/My/Hooks.hs
@@ -1,9 +1,26 @@
-module My.Hooks (myManageHook) where
+module My.Hooks (myManageHook, myStartupHooks) where
 
 import XMonad
-import XMonad.Hooks.ManageHelpers
+  ( ManageHook,
+    X,
+    className,
+    composeAll,
+    doFloat,
+    (-->),
+    (=?),
+  )
+import XMonad.Hooks.ManageHelpers (isDialog)
+import XMonad.Util.SpawnOnce (spawnOnOnce, spawnOnce)
 
 myManageHook :: ManageHook
-myManageHook = composeAll [ className =? "Gimp" --> doFloat
-                          , isDialog --> doFloat
-                          ]
+myManageHook =
+  composeAll
+    [ className =? "Gimp" --> doFloat,
+      isDialog --> doFloat
+    ]
+
+myStartupHooks :: X ()
+myStartupHooks = do
+  spawnOnce "emacs --daemon"
+  spawnOnOnce "workspace3" "slack"
+  spawnOnOnce "workspace3" "discord"
--- dot/xmonad/xmonad-shso.hs
+++ dot/xmonad/xmonad-shso.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import My.Hooks (myManageHook)
+import My.Hooks (myManageHook, myStartupHooks)
 import My.Keys
   ( myKeys,
     myKeysX,
@@ -18,6 +18,7 @@ import XMonad
         logHook,
         manageHook,
         modMask,
+        startupHook,
         terminal
       ),
     xmonad,
@@ -26,6 +27,7 @@ import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Hooks.DynamicLog (dynamicLog)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+import XMonad.Util.SpawnOnce (spawnOnOnce, spawnOnce)
 
 main :: IO ()
 main =
@@ -41,7 +43,9 @@ main =
           terminal = "konsole",
           layoutHook = myLayout,
           manageHook = myManageHook,
-          logHook = dynamicLog
+          logHook = dynamicLog,
+          startupHook = do
+            myStartupHooks
         }
         `additionalKeysP` myKeys
         `additionalKeys` myKeysX
