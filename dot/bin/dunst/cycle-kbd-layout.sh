55b7deef06c3aad61886b572cb4c24107052ef5e
Shahin Sorkh <sorkh.shahin@protonmail.com>
2022-01-15 13:55:40 +0330

    remap caps on every kbd layout change

--- dot/bin/dunst/cycle-kbd-layout.sh
+++ dot/bin/dunst/cycle-kbd-layout.sh
@@ -5,11 +5,11 @@ CURLAYOUT=$(setxkbmap -query | awk '/^layout/ {print $2}')
 
 case "$CURLAYOUT" in
     us)
-        setxkbmap ir
+        setxkbmap -layout ir -option ctrl:nocaps
         dunstify -r 9960 -u low -I "$ICONDIR"/globe-asia.png 'fa'
         ;;
     ir)
-        setxkbmap us
+        setxkbmap -layout us -option ctrl:nocaps
         dunstify -r 9960 -u low -I "$ICONDIR"/globe-americas.png 'en'
         ;;
 esac
