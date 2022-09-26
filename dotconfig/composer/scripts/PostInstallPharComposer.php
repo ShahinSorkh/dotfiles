b62e9cf09192753e4c278d3412bd7f6ce6fb9451
Shahin Sorkh <sorkh.shahin@protonmail.com>
2022-09-26 11:04:26 +0330

    update composer PostInstallPharComposer script

--- dotconfig/composer/scripts/PostInstallPharComposer.php
+++ dotconfig/composer/scripts/PostInstallPharComposer.php
@@ -4,22 +4,22 @@ use Composer\Script\Event;
 
 class PostInstallPharComposer
 {
-	public static function exec(Event $event)
-	{
-		$args = $event->getArguments();
-		if (count($args) === 1) {
-			$args = explode(' as ', $args[0]);
-		}
+    public static function exec(Event $event)
+    {
+        $args = $event->getArguments();
+        if (count($args) === 1) {
+            $args = explode(' as ', $args[0]);
+        }
 
-		$pkg = $args[0];
-		$alias = $args[1] ?? basename($pkg);
+        $pkg = $args[0];
+        $alias = $args[1] ?? basename($pkg);
 
-		self::updatePhar($pkg, $alias);
-	}
+        self::updatePhar($pkg, $alias);
+    }
 
     public static function updatePhar($pkg, $alias)
     {
-		$phar_name = basename($pkg).'.phar';
+        $phar_name = basename($pkg).'.phar';
         rename($phar_name, "vendor/bin/{$alias}");
 
         $composer_json = json_decode(file_get_contents('composer.json'));
@@ -27,10 +27,10 @@ class PostInstallPharComposer
         $phar = "{$pkg} as {$alias}";
 
         if (!in_array($phar, $phars)) {
-        	array_push($phars, $phar);
+            array_push($phars, $phar);
         }
         sort($phars);
 
-        file_put_contents('composer.json', json_encode($composer_json, JSON_PRETTY_PRINT));
+        file_put_contents('composer.json', json_encode($composer_json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
 }
