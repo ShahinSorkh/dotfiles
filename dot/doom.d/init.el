4222b2732496fd3b9bf48096683da693636f1776
Shahin Sorkh <sorkh.shahin@protonmail.com>
2023-06-11 08:29:02 +0330

    enable doom python module

--- dot/doom.d/init.el
+++ dot/doom.d/init.el
@@ -157,7 +157,7 @@
        (php +lsp +tree-sitter)               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp)            ; beautiful is better than ugly
+       (python +lsp +pyright +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
