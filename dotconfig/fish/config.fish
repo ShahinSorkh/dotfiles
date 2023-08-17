f3418007b1efdb564ae8d363d0abf5d8bba28176
Shahin Sorkh <sorkh.shahin@protonmail.com>
2023-08-17 16:19:54 +0330

    add user gem home to fish path

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -1,5 +1,6 @@
 set -U ANDROID_HOME /opt/android-sdk
 
+fish_add_path -g /usr/lib/jvm/default/bin
 fish_add_path -g $HOME/.local/bin
 fish_add_path -g $HOME/.emacs.d/bin
 fish_add_path -g $HOME/.config/composer/vendor/bin
@@ -8,7 +9,7 @@ fish_add_path -g $HOME/.local/share/solana/install/active_release/bin
 fish_add_path -g $HOME/.detaspace/bin
 fish_add_path -g $HOME/.npm-packages/bin
 fish_add_path -g $HOME/opt/VSCode/bin
-fish_add_path -g /usr/lib/jvm/default/bin
+fish_add_path -g (gem environment user_gemhome)/bin || true
 
 set -U fish_greeting
 set -U EDITOR vim
