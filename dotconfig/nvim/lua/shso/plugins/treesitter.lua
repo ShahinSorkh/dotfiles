26d9ced68e545f673a9714a0996d1fb2640d244d
Shahin Sorkh <sorkh.shahin@protonmail.com>
2023-08-17 13:21:35 +0330

    setup nvim lsp properly

--- dotconfig/nvim/lua/shso/plugins/treesitter.lua
+++ dotconfig/nvim/lua/shso/plugins/treesitter.lua
@@ -106,6 +106,7 @@ return {
             vim.opt.foldmethod = "expr"
             vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
             vim.opt.foldenable = true
+            vim.opt.foldlevel = 999
         end
     },
     -- {
